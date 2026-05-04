8b441910cd863cde88fd8a4bf53a6cc78dcd51c0
Tector <Tect0r@gmx.de>
2026-05-04 22:21:08 +0200

    added new data files

--- src/metro_sim/utils/file_loader.py
+++ src/metro_sim/utils/file_loader.py
@@ -7,3 +7,38 @@ def load_balancing() -> dict:
 
     with open(balancing_path, "r", encoding="utf-8") as file:
         return json.load(file)
+    
+def load_map_data() -> dict:
+    project_root = Path(__file__).resolve().parents[3]
+    map_path = project_root / "data" / "map.json"
+
+    with open(map_path, "r", encoding="utf-8") as file:
+        return json.load(file)
+
+def load_buildings_data() -> dict:
+    project_root = Path(__file__).resolve().parents[3]
+    buildings_path = project_root / "data" / "buildings.json"
+
+    with open(buildings_path, "r", encoding="utf-8") as file:
+        return json.load(file)
+    
+def load_buildings_effects_data() -> dict:
+    project_root = Path(__file__).resolve().parents[3]
+    buildings_path = project_root / "data" / "buildings_effects.json"
+
+    with open(buildings_path, "r", encoding="utf-8") as file:
+        return json.load(file)
+    
+def load_buildings_cost_data() -> dict:
+    project_root = Path(__file__).resolve().parents[3]
+    buildings_path = project_root / "data" / "buildings_cost.json"
+
+    with open(buildings_path, "r", encoding="utf-8") as file:
+        return json.load(file)
+    
+def load_initial_data() -> dict:
+    project_root = Path(__file__).resolve().parents[3]
+    buildings_path = project_root / "data" / "initial.json"
+
+    with open(buildings_path, "r", encoding="utf-8") as file:
+        return json.load(file)
