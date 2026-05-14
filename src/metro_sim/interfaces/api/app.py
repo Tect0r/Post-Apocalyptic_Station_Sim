68275d6e6ee30163a736b7cddf3f7e810a0bcfe9
Tector <Tect0r@gmx.de>
2026-05-14 19:03:16 +0200

    fix for login

--- src/metro_sim/interfaces/api/app.py
+++ src/metro_sim/interfaces/api/app.py
@@ -26,9 +26,11 @@ def create_app() -> FastAPI:
         allow_origins=[
             "http://localhost:5173",
             "http://127.0.0.1:5173",
+            "http://localhost:5174",
+            "http://127.0.0.1:5174",
         ],
-        allow_credentials=True,
-        allow_methods=["*"],
+        allow_credentials=False,
+        allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
         allow_headers=["*"],
     )
 
